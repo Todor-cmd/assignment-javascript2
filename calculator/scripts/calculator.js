caa838f786b59722340f170d1051b02011f1f002
Todor-cmd <todor.malto.mladenovic@gmail.com>
2022-12-09 16:47:54 +0100

    Adding comments

--- calculator/scripts/calculator.js
+++ calculator/scripts/calculator.js
@@ -1,4 +1,6 @@
+
 //----------------------------------------------Global Variables--------------------------------------------------------//
+
 const allButtons = document.querySelectorAll(`button`);
 const digits = document.querySelectorAll(`#digits > button`);
 const operations = document.querySelectorAll(`#operations > button`);
@@ -10,9 +12,12 @@ const backspaceButton = document.querySelector(`#backspace`);
 
 let input = [];
 const operatorList = [`+`, `-`, `*`, `/`];
+
+//This will be used to know whether a new digit input should reset the calculator
 let isPreviousResult = false;
 
 //-------------------------------------------------Event listeners------------------------------------------------------//
+
 allButtons.forEach((button) => {
     button.addEventListener(`click`, isPreviousError);
 });
@@ -53,22 +58,29 @@ document.addEventListener(`keyup`, (event) => {
     else if (event.key === `Backspace`) {
         backspaceFunctionality();
     }
-    else if (event.key === `Clear`) {
+    else if (event.key === `Clear` || event.key == `Delete`) {
         clearFunctionality();
     }
 })
 
 //-------------------------------------------------Event Listener Functions----------------------------------------------//
+
+/**
+ * Calculates the result of the first operation if there are at least 3 inputs
+ */
 function compute() {
     if (input.length >= 3) {
         isPreviousResult = true;
         try {
+            //The result of the operation is rounded to two decimal places
             let result = Math.round(operate(input[1], input[0], input[2]) * 100) / 100;
             input = [result];
             displayResult(result);
             
         }
         catch (e) {
+            //This clears the input and displays the error message
+             //Also the dot is enabled to allow use of it(incase it was disabled)
             input=[];
             dotButton.addEventListener(`click`, dotFunctionality);
             displayP.textContent = e;
@@ -76,6 +88,9 @@ function compute() {
     }
 }
 
+/**
+ * Resets the calculator, both the display and memory(input variable).
+ */
 function clearFunctionality() {
     isPreviousResult = false;
     dotButton.addEventListener(`click`, dotFunctionality);
@@ -83,6 +98,9 @@ function clearFunctionality() {
     input = [];
 }
 
+/**
+ * Performs the functionality of the dot in arithmatics.
+ */
 function dotFunctionality() {
     if (isPreviousResult) {
         displayP.textContent = ``;
@@ -110,34 +128,86 @@ function dotFunctionality() {
     displayP.insertAdjacentText('beforeend', dotButton.textContent);
 }
 
+/**
+ * Performs the functionality of the backspace.
+ */
 function backspaceFunctionality() {
     if (input.length === 0){
         return;
     }
 
     let previous = input.pop();
+
+    //If the previous element in display is the result of user pressing '=', then delete entire element.
     if (isPreviousResult){
         displayP.textContent = displayP.textContent.slice(0, -previous.length);
         dotButton.addEventListener(`click`, dotFunctionality);
     } else {
+
+        //If previous element in display is a dot, enable dot again.
         if (previous.slice(-1, previous.length) === `.` || previous === `.`) {
-            console.log(`hello`)
             dotButton.addEventListener(`click`, dotFunctionality);
         }
         displayP.textContent = displayP.textContent.slice(0, -1);
+
+        //If element being deleted is part of a number with multiple digits,
+        //push back the number without deleted element to memory
         if (previous.length > 1){
             input.push(previous.slice(0, -1));
         }
     } 
 }
 
+/**
+ * Checks if an error message is displayed and if so clears the message.
+ */
 function isPreviousError() {
     if (displayP.textContent.slice(0,5) === `Error`) {
         displayP.textContent = ``;
     }
 }
 
+/**
+ * Performs the functionality of the digit.
+ * @param {String} digit - A digit thats been entered by user.
+ */
+function digitFunctionality(digit) {
+    //Clears calculator if displayed element is result of user pressing '='
+    if (isPreviousResult) {
+        displayP.textContent = ``;
+        input = [];
+    } 
+
+    isPreviousResult = false;
+    addInput(`${digit}`);
+    displayP.insertAdjacentText('beforeend', `${digit}`);
+}
+
+/**
+ * Performs the functionality of the operator.
+ * @param {String} operator - An operator thats been entered by the user. 
+ */
+function operationFunctinoality(operator) {
+    dotButton.addEventListener(`click`, dotFunctionality);
+    isPreviousResult = false;
+    try {
+        earlyComputeCheck();
+        addInput(operator);
+        displayP.insertAdjacentText('beforeend', operator);
+    } catch (e) {
+        input=[];
+        displayP.textContent = e;
+    }
+}
+
+
 //-------------------------------------------------Helper functions-----------------------------------------------------//
+
+/**
+ * Adds new input by user to the input array(used as short-term memory).
+ * @param {String} newInput - The new input of the user.
+ * @throws - Errors if first input is an operator or two consecutive operators are entered.
+ */
 function addInput(newInput) {
     if(input.length == 0 && !operatorList.includes(newInput)) {
         input.push(newInput);
@@ -162,6 +232,9 @@ function addInput(newInput) {
     }
 }
 
+/**
+ * Checks if there are already 3 elements in input, and computes if so.
+ */
 function earlyComputeCheck() {
     if (input.length > 2) {
         compute();
@@ -169,10 +242,21 @@ function earlyComputeCheck() {
     }
 }
 
+/**
+ * Displays the result in the calulator display.
+ * @param {*} result - the result to display. 
+ */
 function displayResult(result) {
     displayP.textContent = result;
 }
 
+/**
+ * Performs the chosen operation with the chosen operands of the user.
+ * @param {String} operator - The chosen operator.
+ * @param {String} operandL - The chosen operand for the left side.
+ * @param {String} operandR - The chosen operand for the right side.
+ * @returns {float} - The result of the operations.
+ */
 function operate(operator, operandL, operandR) {
     switch (operator) {
         case `+`: return add(parseFloat(operandL), parseFloat(operandR));
@@ -182,43 +266,44 @@ function operate(operator, operandL, operandR) {
     }
 }
 
-function digitFunctionality(digit) {
-    if (isPreviousResult) {
-        displayP.textContent = ``;
-        input = [];
-    } 
-
-    isPreviousResult = false;
-    addInput(`${digit}`);
-    displayP.insertAdjacentText('beforeend', `${digit}`);
-}
-
-function operationFunctinoality(operator) {
-    dotButton.addEventListener(`click`, dotFunctionality);
-    isPreviousResult = false;
-    try {
-        earlyComputeCheck();
-        addInput(operator);
-        displayP.insertAdjacentText('beforeend', operator);
-    } catch (e) {
-        input=[];
-        displayP.textContent = e;
-    }
-}
-
 //---------------------------------------------------Simple Mathematical functions--------------------------------------//
+
+/**
+ * Adds the left operand to the right operand.
+ * @param {float} operandL - The chosen operand for the left side.
+ * @param {float} operandR - The chosen operand for the right side.
+ * @returns {float} - The result of the addition.
+ */
 function add(operandL, operandR) {
     return operandL + operandR;
 }
 
+/**
+ * Subtracts the left operand from the right operand.
+ * @param {float} operandL - The chosen operand for the left side.
+ * @param {float} operandR - The chosen operand for the right side.
+ * @returns {float} - The result of the subtraction.
+ */
 function subtract(operandL, operandR) {
     return operandL - operandR;
 }
 
+/**
+ * Multiplies the left operand to the right operand.
+ * @param {float} operandL - The chosen operand for the left side.
+ * @param {float} operandR - The chosen operand for the right side.
+ * @returns {float} - The result of the multiplication.
+ */
 function multiply(operandL, operandR) {
     return operandL * operandR;
 }
 
+/**
+ * Divides the left operand by the right operand.
+ * @param {float} operandL - The chosen operand for the left side.
+ * @param {float} operandR - The chosen operand for the right side.
+ * @returns {float} - The result of the division.
+ */
 function divide(operandL, operandR) {
     if (operandR === 0){
         throw `Error: Can't divide by zero. Input has been cleared.`;
